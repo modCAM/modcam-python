be36621dbda5aaa5b1647ea2dd384eef39589ca6
prosilio <prosilio.github@proton.me>
2026-03-14 12:35:59 -0600

    Use apt-get to install vcpkg dependencies in cibw
    I don't know why I was using dnf.

--- tools/cibw/cibw-install-tools-linux.sh
+++ tools/cibw/cibw-install-tools-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # vcpkg
-dnf install -y curl zip unzip tar
+apt-get install -y curl zip unzip tar
 git clone https://github.com/microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh -disableMetrics
